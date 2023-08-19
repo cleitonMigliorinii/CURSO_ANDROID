5b56a07b5c6e3b62a72215ce06efbcf2cc14400a
Cleiton Migliorini <cleitonMigliorinii@users.noreply.github.com>
2023-08-19 11:29:29 -0300

    Trabalhando com repetição

---
+++ INTRODUCAO_KOTLIN/src/main/kotlin/AtividadeRepeticao.kt
@@ -0,0 +1,8 @@
+fun main() {
+
+    println()
+    for (i in 0..10){
+        println(" 7 x ${i} = ${7*i}")
+    }
+
+}
---
+++ INTRODUCAO_KOTLIN/src/main/kotlin/MainRepeticao.kt
@@ -0,0 +1,20 @@
+fun main() {
+
+
+    repeat(23) {
+        print("-")
+    }
+
+    println()
+    for (i in 1..5){
+        println(i)
+    }
+
+    val colecao = 1..9
+
+    for (item in colecao){
+        println(item)
+    }
+
+
+}
