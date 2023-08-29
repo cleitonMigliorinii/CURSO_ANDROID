c4f3848aee84449a07e70fe72e71ee266d092a61
Cleiton Migliorini <cleitonMigliorinii@users.noreply.github.com>
2023-08-28 21:34:52 -0300

    Criando funcionalidade do calculo do IMC

--- CalculaIMC_Kotlin/app/src/main/java/br/edu/utfpr/calculaimc_kotlin/MainActivity.kt
+++ CalculaIMC_Kotlin/app/src/main/java/br/edu/utfpr/calculaimc_kotlin/MainActivity.kt
@@ -2,10 +2,74 @@ package br.edu.utfpr.calculaimc_kotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import kotlin.math.pow
 
 class MainActivity : AppCompatActivity() {
+
+    /*
+    * lateinit é uma palavra-chave usada para declarar uma propriedade
+    * não anulável (non-nullable) cujo valor será atribuído posteriormente
+    * */
+    private lateinit var etPeso: EditText;
+    private lateinit var etAltura : EditText;
+    private lateinit var tvResultado : TextView
+    private lateinit var btCalcular : Button
+    private lateinit var btLimpar : Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        etPeso = findViewById( R.id.etPeso )
+        etAltura = findViewById( R.id.etAltura )
+        tvResultado = findViewById( R.id.tvResultado )
+        btCalcular = findViewById( R.id.btCalcular )
+        btLimpar = findViewById( R.id.btLimpar )
+
+        btCalcular.setOnClickListener {
+            btCalcularOnClick()
+        }
+
+        btLimpar.setOnClickListener {
+            btLimparOnClick()
+        }
+
+        btLimpar.setOnLongClickListener {
+            Toast.makeText( this, "Botão para Limpar", Toast.LENGTH_SHORT ).show()
+            false
+        }
     }
+
+    private fun btLimparOnClick() {
+        etPeso.setText( "" )
+        etAltura.setText( "" )
+        tvResultado.text = "0.0"
+        etPeso.requestFocus()
+    }
+
+    private fun btCalcularOnClick() {
+
+        if ( etPeso.text.toString().isEmpty() ) {
+            etPeso.error = "Campo peso deve ser preenchido"
+            return
+        }
+
+        if ( etAltura.text.toString().isEmpty() ) {
+            etAltura.error = "Campo altura deve ser preenchido"
+            return
+        }
+
+        val peso = etPeso.text.toString().toDouble()
+        val altura = etAltura.text.toString().toDouble()
+
+        val imc = peso / altura.pow( 2.0 )
+
+        tvResultado.text = "%.1f".format( imc )
+    }
+
+
+
 }
