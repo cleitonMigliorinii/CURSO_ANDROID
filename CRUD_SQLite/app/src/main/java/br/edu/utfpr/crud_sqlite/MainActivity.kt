082c8817d2cdb8a9e284ef079fd48d597d4ff724
Cleiton Migliorini <cleitonMigliorinii@users.noreply.github.com>
2023-08-31 13:04:34 -0300

    Criado os metodos

--- CRUD_SQLite/app/src/main/java/br/edu/utfpr/crud_sqlite/MainActivity.kt
+++ CRUD_SQLite/app/src/main/java/br/edu/utfpr/crud_sqlite/MainActivity.kt
@@ -2,10 +2,17 @@ package br.edu.utfpr.crud_sqlite
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    fun btListarOnClick(view: View) {}
+    fun btPesquisarOnClick(view: View) {}
+    fun btExcluirOnClick(view: View) {}
+    fun btAlterarOnClick(view: View) {}
+    fun btIncluirOnClick(view: View) {}
 }
