95c65652a428d82641ead99780ee29f473fd72de
Cleiton Migliorini <cleitonMigliorinii@users.noreply.github.com>
2023-09-01 13:29:55 -0300

    Mapeando os campos no MainActivity.kt

--- ATIVIDADE_ABASTECIMENTO/app/src/main/java/br/edu/utfpr/atividade_abastecimento/MainActivity.kt
+++ ATIVIDADE_ABASTECIMENTO/app/src/main/java/br/edu/utfpr/atividade_abastecimento/MainActivity.kt
@@ -2,10 +2,20 @@ package br.edu.utfpr.atividade_abastecimento
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var etCodigoCombustivel: EditText;
+    private lateinit var etCidadeAbastecimento: EditText;
+    private lateinit var etQuantidadeLitros: EditText;
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        etCodigoCombustivel = findViewById(R.id.etCodigoCombustivel)
+        etCidadeAbastecimento = findViewById(R.id.etCidadeAbastecimento)
+        etQuantidadeLitros = findViewById(R.id.etQuantidadeLitros)
     }
 }
