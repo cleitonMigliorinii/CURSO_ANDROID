d9d7a50d5b727e2be44ee8f6b4ad409fa133ec05
Cleiton Migliorini <cleitonMigliorinii@users.noreply.github.com>
2023-09-02 10:30:46 -0300

    Criado tela inicial

--- TrocaTela/app/src/main/java/br/edu/utfpr/trocatela/MainActivity.kt
+++ TrocaTela/app/src/main/java/br/edu/utfpr/trocatela/MainActivity.kt
@@ -2,10 +2,16 @@ package br.edu.utfpr.trocatela
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun btLancamentoInicialOnClick(view: View) {
+
+
     }
 }
