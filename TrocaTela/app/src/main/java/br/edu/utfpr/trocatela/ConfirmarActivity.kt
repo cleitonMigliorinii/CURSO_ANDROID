3785a311f953f310a3a9de2b570e5fe076db2f1b
Cleiton Migliorini <cleitonMigliorinii@users.noreply.github.com>
2023-09-02 11:42:09 -0300

    Trabalhando com compartilhamento de dados entre telas

--- TrocaTela/app/src/main/java/br/edu/utfpr/trocatela/ConfirmarActivity.kt
+++ TrocaTela/app/src/main/java/br/edu/utfpr/trocatela/ConfirmarActivity.kt
@@ -2,10 +2,24 @@ package br.edu.utfpr.trocatela
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.TextView
 
 class ConfirmarActivity : AppCompatActivity() {
+
+    private  lateinit var  etCode: TextView;
+    private  lateinit var  etQtd: TextView;
+    private  lateinit var  etValor: TextView;
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_confirmar)
+
+        etCode = findViewById(R.id.etCodConfirmar)
+        etQtd = findViewById(R.id.etQtdeConfirmar)
+        etValor = findViewById(R.id.etValorConfirmar)
+
+        etCode.setText(intent.getStringExtra("cod"))
+        etQtd.setText(intent.getStringExtra("qtd"))
+        etValor.setText(intent.getStringExtra("valor"))
     }
 }
--- TrocaTela/app/src/main/java/br/edu/utfpr/trocatela/LancamentoActivity.kt
+++ TrocaTela/app/src/main/java/br/edu/utfpr/trocatela/LancamentoActivity.kt
@@ -4,16 +4,29 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.EditText
 
 class LancamentoActivity : AppCompatActivity() {
+    private  lateinit var  etCode: EditText;
+    private  lateinit var  etQtd: EditText;
+    private  lateinit var  etValor: EditText;
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_lancamento)
+
+        etCode = findViewById(R.id.etCodLancamento)
+        etQtd = findViewById(R.id.etQtdeLancamento)
+        etValor = findViewById(R.id.etValorLancamento)
     }
 
     fun limparLancamentoOnClick(view: View) {}
     fun btConfirmarLancamento(view: View) {
         val intent = Intent(this, ConfirmarActivity::class.java)
+        intent.putExtra("cod", etCode.text.toString())
+        intent.putExtra("qtd", etQtd.text.toString())
+        intent.putExtra("valor", etValor.text.toString())
         startActivity(intent)
     }
 }
